4cfcdeed37d8ba0a923e8d3f454ac835db210491
Prajwal Nayak <prajwal.unayak@gmail.com>
2025-06-02 09:48:14 +0530

    Add files via upload

---
+++ Assignment16/16_01.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+
+void Pattern(int iRow, int iCol)
+{
+    int i = 0;
+    int j = 0;
+    int icnt = 1;
+   for(i=1;i<=iRow;i++)
+   {
+    for(j=1;j<=iCol;j++)
+    {
+       // if(icnt >=9)
+        if(i>=j)
+        {
+            printf("*\t");
+        }
+        else
+        {
+            printf(" \t");
+        }
+    }
+   // ch++;
+    printf("\n");
+   }
+}
+
+int main()
+{
+    int iVal1 = 0, iVal2 = 0;
+
+    printf("Enter no of rows: ");
+    scanf("%d", &iVal1);
+
+    printf("Enter no of columns: ");
+    scanf("%d", &iVal2);
+
+    Pattern(iVal1, iVal2);
+
+    return 0;
+}
